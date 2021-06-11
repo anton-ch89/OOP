095ff29c927558dcb06c5526baeacc6b2a607081
Anton Chudinov <anton.ch89@gmail.com>
2021-06-11 21:07:18 +0500

    Урок ООП

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let body = document.querySelector('body');
+
+function DomElement (selector, height, width, bg, fontSize) {
+this.selector = selector;
+this.height = height;
+this.width = width;
+this.bg = bg;
+this.fontSize = fontSize;
+this.create();
+}
+
+DomElement.prototype.create = function () {
+    let newDiv = document.createElement('div');
+    if(this.selector[0] === '.'){
+        newDiv.classList.add(this.selector.slice(1));
+        body.append(newDiv);
+    }else if(this.selector[0] === '#'){
+        newDiv.id = this.selector.slice(1);
+        body.append(newDiv);
+}
+newDiv.style.height = this.height + 'px';
+newDiv.style.width = this.width + 'px';
+newDiv.style.background  = this.bg;
+newDiv.style.fontSize  = this.fontSize + 'px';
+newDiv.textContent = 'Новый элемент';
+
+};
+const domEl = new DomElement('#block', 400, 400, 'red', 36);
+
