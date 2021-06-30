7c0ddfa3023efd66622850accde7a699111b755f
Anton Chudinov <anton.ch89@gmail.com>
2021-06-30 20:09:36 +0500

    Урок 14 доп

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
+window.addEventListener("DOMContentLoaded", function (){
+
 "use strict";
 
 let body = document.querySelector('body');
+body.style.margin = '0';
 
-function DomElement (selector, height, width, bg, fontSize) {
+class DomElement { 
+constructor (selector, height, width, bg, fontSize) {
 this.selector = selector;
 this.height = height;
 this.width = width;
 this.bg = bg;
 this.fontSize = fontSize;
-
 }
 
-DomElement.prototype.create = function () {
+
+create () {
     let newDiv = document.createElement('div');
     if(this.selector[0] === '.'){
         newDiv.classList.add(this.selector.slice(1));
@@ -24,11 +28,46 @@ newDiv.style.cssText = `height: ${this.height}px;
 width: ${this.width}px;
 background: ${this.bg};
 font-size: ${this.fontSize}px;
+position: absolute;
 `;
 
 newDiv.textContent = 'Новый элемент';
 
-};
-const domEl = new DomElement('#block', 400, 400, 'red', 36);
+
+
+}
+moving  () {
+    const elem = document.querySelector('div');
+    let counterX = 0,
+      counterY = 0;
+    document.onkeydown =(e)=>{
+
+        if(e.key === 'ArrowRight') {
+            counterX += 10;
+            elem.style.left = counterX + 'px';
+        }
+        else if(e.key === 'ArrowLeft') {
+            counterX -= 10;
+            elem.style.left = counterX + 'px';
+        }
+        else if(e.key === 'ArrowUp') {
+            counterY -= 10;
+            elem.style.top = counterY + 'px';
+        }
+        else if(e.key === 'ArrowDown') {
+            counterY +=10;
+            elem.style.top =  counterY + 'px';
+            
+            
+        }
+    };
+
+}
+}
+
+const domEl = new DomElement('#block', 100, 100, 'blue', 14);
 
 domEl.create();
+domEl.moving();
+
+});
